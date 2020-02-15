6f708f539a45edf04c83e3c791292543b4306e07
nicholascourage <48243052+nicholascourage@users.noreply.github.com>
2020-02-15 20:23:49 +0000

    69. list jobs from database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,26 @@
                <th></th>
            </thead>
            <tbody>
-               @for($i=0;$i<10;$i++)
+               @foreach($jobs as $job)
                <tr>
                    <td><img src="{{asset('avatar/default-avatar-img.jpg')}}" width="80px"></td>
-                   <td>Position: Web Developer</td>
-                   <td><i class="fas fa-map-marker" aria-hidden="true"></i> Cornwall</td>
-                   <td>Closing Date: 2020-03-04</td>
+                   <td>Position: {{$job->position}}
+                       <br>
+                       <i class="fas fa-clock" aria-hidden="true"></i>&nbsp; {{$job->type}}
+                   </td>
+                   <td><i class="fas fa-map-marker" aria-hidden="true"></i> {{$job->address}}</td>
+                   <td><i class="fas fa-calendar" aria-hidden="true"></i>&nbsp; Date: {{$job->created_at->diffForHumans()}}</td>
                    <td><button class="btn btn-success btn-sm">Apply</button></td>
                </tr>
-               @endfor
+               @endforeach
            </tbody>
        </table>
     </div>
 </div>
 @endsection
+<style>
+    .fas{
+
+        color: #4183D7;
+    }
+</style>
