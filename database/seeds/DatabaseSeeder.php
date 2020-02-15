c7fac8869eb516a85edf7a34eab250ea03164cd4
nicholascourage <48243052+nicholascourage@users.noreply.github.com>
2020-02-15 18:54:07 +0000

    67. make seeder file for users,jobs and companies table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory('App\User',20)->create();
+        factory('App\Company',20)->create();
+        factory('App\Job',20)->create();
+
     }
 }
