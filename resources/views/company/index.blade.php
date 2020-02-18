d4b246ad6ec1358c267a5ad5b1796273e7709f3b
nicholascourage <48243052+nicholascourage@users.noreply.github.com>
2020-02-18 15:22:11 +0000

    74. company page design part3 and fetch jobs posted by company

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -12,6 +12,31 @@
                 <p>Slogan-{{$company->slogan}}&nbsp; Address-{{$company->address}}&nbsp; Phone-{{$company->phone}}&nbsp; Website-{{$company->website}}&nbsp;</p>
             </div>
         </div>
+        <table class="table">
+           <thead>
+               <th></th>
+               <th></th>
+               <th></th>
+               <th></th>
+               <th></th>
+           </thead>
+           <tbody>
+               @foreach($company->jobs as $job)
+               <tr>
+                   <td><img src="{{asset('avatar/default-avatar-img.jpg')}}" width="80px"></td>
+                   <td>Position: {{$job->position}}
+                       <br>
+                       <i class="fas fa-clock" aria-hidden="true"></i>&nbsp; {{$job->type}}
+                   </td>
+                   <td><i class="fas fa-map-marker" aria-hidden="true"></i> {{$job->address}}</td>
+                   <td><i class="fas fa-calendar" aria-hidden="true"></i>&nbsp; Date: {{$job->created_at->diffForHumans()}}</td>
+                   <td>
+                       <a href="{{route('jobs.show', [$job->id, $job->slug])}}"><button class="btn btn-success btn-sm">Apply</button></a>
+                    </td>
+               </tr>
+               @endforeach
+           </tbody>
+       </table>
     </div>
 </div>    
 
