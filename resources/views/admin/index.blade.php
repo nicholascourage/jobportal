665eae0c6fa53afd882c75276d17af9e09e96f7f
nicholascourage <48243052+nicholascourage@users.noreply.github.com>
2020-03-18 15:32:13 +0000

    144. Blog post part1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,53 @@
-dashboard
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Menu</div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="">Home</a></li>
+                        <li class="list-group-item"><a href="">Home</a></li>
+                        <li class="list-group-item"><a href="">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" name="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Title</label>
+                            <textarea name="content" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Image</label>
+                            <input type="file" name="image" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Status</label>
+                            <select name="status" class="form-control">
+                                <option value="1">Live</option>
+                                <option value="0">Draft</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
